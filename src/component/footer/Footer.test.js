3ace32b77dbe3c22301f4fe17265ea89dcd1552a
Lau,Joey K.W <j.lau@student.fontys.nl>
2020-11-11 14:29:46 +0100

    Added footer testing

--- src/component/footer/Footer.test.js
+++ src/component/footer/Footer.test.js
@@ -5,44 +5,54 @@ import {configure, mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({adapter: new Adapter()});
-
-// describe('test enzyme', () => {
-// 	test('should toggle the play state', () => {
-// 		const songData = {
-// 			accountId: 18,
-// 			artistName: "Akie??",
-// 			dateAdded: "26-10-2020",
-// 			duration: "4:32",
-// 			name: "Ama no Zaku",
-// 			songId: 25,
-// 		}
-// 		const wrapper = mount(<Footer currentSong={songData}/>);
-
-// 		wrapper.find('#play').simulate('click');
-		
-// 		// expect(wrapper.find('.song-name').text()).toContain(songData.name)
-// 		// expect(wrapper.state('songPlaying')).to.equal(true);
-// 	})
-	
-// })
-
 afterEach(cleanup)
 
-test('Renders song data', () => {
-	const songData = {
-		accountId: 18,
-		artistName: "Akie??",
-		dateAdded: "26-10-2020",
-		duration: "4:32",
-		name: "Ama no Zaku",
-		songId: 25,
-	}
+beforeAll(() => {
+	window.HTMLMediaElement.prototype.load = () => {};
+	window.HTMLMediaElement.prototype.play = () => {};
+	window.HTMLMediaElement.prototype.pause = () => {};
+	window.HTMLMediaElement.prototype.addTextTrack = () => {};
+})
 
-	const { getByTestId } = render(<Footer currentSong={songData}/>);
-	const songName = getByTestId("song-name");
-	const artistName = getByTestId("artist-name");
+const songData = {
+	accountId: 18,
+	artistName: "artistName",
+	dateAdded: "26-10-2020",
+	duration: "4:32",
+	name: "songName",
+	songId: 25,
+}
 
-	expect(songName).toHaveTextContent(songData.name);
-	expect(artistName).toHaveTextContent(songData.artistName);
-});
+describe('Load and show song correctly', () => {
+	test('Renders song data', () => {
+
+		const { getByTestId } = render(<Footer currentSong={songData}/>);
+		const songName = getByTestId("song-name");
+		const artistName = getByTestId("artist-name");
+	
+		expect(songName).toHaveTextContent(songData.name);
+		expect(artistName).toHaveTextContent(songData.artistName);
+	});
+
+		
+	test('Should update state with new song', () => {
+		const wrapper = mount(<Footer currentSong={songData}/>);
+		expect(wrapper.state().song).toEqual(songData);
+	})
+	
+})
+
+describe('Play music on click', () => {
+	const wrapper = mount(<Footer currentSong={songData}/>);
+	
+	
+	test('should toggle the play state', () => {
+		wrapper.find('#play').simulate('click');
+		expect(wrapper.state().songPlaying).toEqual(true);
+
+		wrapper.find('#play').simulate('click');
+		expect(wrapper.state().songPlaying).toEqual(false);
+	})
+
+})
 
